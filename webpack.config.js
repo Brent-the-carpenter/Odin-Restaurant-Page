3c7ee3c3cbc8ee7889707fbe6d59001eff4af491
Brent-the-carpenter <beh337@gmail.com>
2024-01-13 22:05:06 -0500

    installed all dev dependencies needed!

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,42 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./src",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Spores Pizza",
+      template: "./src/index.html",
+      inject: "head",
+      scriptLoading: "defer",
+    }),
+  ],
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
